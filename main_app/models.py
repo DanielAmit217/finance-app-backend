e496ea39f3ad2f5153bce5b1360d429a87595bc9
Roy Levinbook <roi.levinbook@gmail.com>
2025-12-07 16:30:51 +0200

    עקנג

--- main_app/models.py
+++ main_app/models.py
@@ -1,12 +1,66 @@
 from django.db import models
+import uuid
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 from simple_history.models import HistoricalRecords
 from djmoney.models.fields import MoneyField
 
 # Create your models here.
 
+#  הורשה ממודל USER
+class User(AbstractUser): 
+    username=None
+    email=None 
+    password=None
+    #מזהה מערכת פנימי KEY ייחודי
+    app_id = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
+    #הגדרת טלפון כמזהה ייחודי וברירת מחדל להתחברות
+    phone = models.CharField(max_length=10, unique=True)
+    USERNAME_FIELD="phone"
+    REQUIRED_FIELDS=[]
+    preferred_currency = models.CharField(max_length=5, default="ILS")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    plan = models.CharField(
+        max_length=20,
+        choices=[
+            ("free", "Free"),
+            ("early_pro", "Early PRO"),
+            ("pro", "PRO"),
+        ],
+        default="free"
+    )
+# צריך להסויף עוד מכשירים פינסים אחר כך
+class Account(models.Model):
+    ACCOUNT_TYPES = (
+        ("bank", "שם בנק"),
+        ("credit_card", "כרטיסי אשראי"),
+        ("investment_account","חשבון השקעות")
+        ("pension_account","ביטוח פנסיה"))
+     
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,related_name="accounts"
+)
+    account_type = models.CharField(
+        max_length=20,
+        choices=ACCOUNT_TYPES,
+    )
+    #bank_name = models.CharField(max_length=50, blank=True)
+    initial_balance = MoneyField(
+        max_digits=10, decimal_places=2, default_currency="ILS"
+    )
+    current_balance = models.DecimalField(
+        max_digits=14,
+        decimal_places=2,
+        default=0)
+    history = HistoricalRecords()
+    last_synced = models.DateTimeField(blank=True, null=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
 
+   
 class Transaction(models.Model):
     INCOME = "in"
     EXPENSE = "ex"
@@ -49,13 +103,3 @@ class SourceFile(models.Model):
     updated_at = models.DateTimeField(default=timezone.now)
 
 
-class Account(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=50)
-    bank_name = models.CharField(max_length=50, blank=True)
-    initial_balance = MoneyField(
-        max_digits=10, decimal_places=2, default_currency="USD"
-    )
-    history = HistoricalRecords()
-    created_at = models.DateTimeField(default=timezone.now)
-    updated_at = models.DateTimeField(default=timezone.now)
